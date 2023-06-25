911082776827c1f58daf345400edecd1d9e2b28d
shijingjing <shijingjing07@126.com>
2023-06-25 15:12:47 +0800

    IPy

--- base_t/ip_t.py
+++ base_t/ip_t.py
@@ -1,10 +1,36 @@
 import IPy
 
 
-s = "11.31.71.1,11.31.72.225,11.31.72.97,11.31.88.1,11.31.180.25,11.31.180.33,11.31.180.129,11.31.180.161,11.31.180.1,11.31.180.97.11.31.180.65,11.31.180.193,11.31.180.209,11.31.180.217,11.31.180.225,11.31.181.33,11.31.181.1,11.31.182.225"
-ips = s.split(',')
-for ip in ips:
+def check_ip(ip):
     try:
         IPy.IP(ip)
-    except Exception as ex:
-        print(f"ip error: {ip}")
+    except Exception:
+        print(f"not ip error: {ip}")
+
+
+# net转IP列表
+def IPSplitBlocks(ip2ip_str):
+    """
+    ip范围转换为ip列表，不包括网络号0、广播地址255
+    """
+    if '/' in ip2ip_str:
+        ips = IPy.IP(ip2ip_str, make_net=1)
+        ipx = [ips[0].net().strNormal(), ips[-1].net().strNormal()]
+    elif '-' in ip2ip_str:
+        ipx = ip2ip_str.split('-')
+    else:
+        # 排查 广播地址 网络号
+        if ip2ip_str.endswith('.0') or ip2ip_str.endswith('.255'):
+            return []
+        return [ip2ip_str]
+    ip2num = lambda x: sum([256 ** i * int(j) for i, j in enumerate(x.split('.')[::-1])])
+    num2ip = lambda x: '.'.join([str(x // (256 ** i) % 256) for i in range(3, -1, -1)])
+    a = [num2ip(i) for i in range(ip2num(ipx[0]), ip2num(ipx[1]) + 1) if not ((i + 1) % 256 == 0 or (i) % 256 == 0)]
+    return a
+
+
+if __name__ == "__main__":
+    # check_ip(ip="127.0.0.258")
+    r = IPSplitBlocks("172.16.0.0/24")
+    print(len(r))
+    print(r)
