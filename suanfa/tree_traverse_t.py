db6316d181c08087af98a0354d7a7bf4d2f18af8
shijingjing <shijingjing07@126.com>
2023-07-13 18:17:15 +0800

    tree traverse

---
+++ suanfa/tree_traverse_t.py
@@ -0,0 +1,88 @@
+class Tree:
+    def __init__(self, val, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def pre_traverse_t(self, root: Tree) -> list[int]:
+        res = []
+        if not root:
+            return res
+
+        def pre(tree, res):
+            res.append(tree.val)
+            if tree.left:
+                pre(tree.left, res)
+            if tree.right:
+                pre(tree.right, res)
+
+        pre(root, res)
+        return res
+
+    def middle_traverse_t(self, root):
+        res = []
+        if not root:
+            return res
+
+        def middle(tree, res):
+            if tree.left:
+                middle(tree.left, res)
+            res.append(tree.val)
+            if tree.right:
+                middle(tree.right, res)
+
+        middle(root, res)
+        return res
+
+    def post_traverse_t(self, root):
+        res = []
+        if not root:
+            return res
+
+        def post(tree, res):
+            if tree.left:
+                post(tree.left, res)
+            if tree.right:
+                post(tree.right, res)
+            res.append(tree.val)
+
+        post(root, res)
+        return res
+
+    def level_traverse_t(self):
+        pass
+
+
+if __name__ == '__main__':
+    """
+         1
+    2          3
+4       5   6      7
+pre: 1 2 4 5 3 6 7
+mid: 4 2 5 1 8 6 3 7
+post: 4 5 2 6 7 3 1
+    """
+    t4 = Tree(4)
+    t5 = Tree(5)
+    t6 = Tree(6)
+    t7 = Tree(7)
+    t2 = Tree(2)
+    t3 = Tree(3)
+    t1 = Tree(1)
+
+    t1.left = t2
+    t1.right = t3
+
+    t2.left = t4
+    t2.right = t5
+
+    t3.left = t6
+    t3.right = t7
+
+    r = Solution().pre_traverse_t(None)
+    print(r)
+    # Solution().middle_traverse_t(t1)
+    # Solution().post_traverse_t(t1)
+
