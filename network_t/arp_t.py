a452a4ee4ae9b9d2231d4c8252be134397d1d5e5
shijingjing <shijingjing07@126.com>
2023-11-30 11:03:46 +0800

    scapy发送arp请求包

--- network_t/arp_t.py
+++ network_t/arp_t.py
@@ -25,10 +25,29 @@ def remote_mac(dst_ip):
     return dst_mac
 
 
+def arp_request_t():
+    """
+    arp请求远程主机
+    :return:
+    """
+    from scapy.all import Ether, ARP, srp
+
+    # 构造 ARP 请求包
+    packet = Ether(dst="ff:ff:ff:ff:ff:ff") / ARP(op=1, pdst="192.168.2.200")
+
+    # 发送 ARP 请求并接收响应
+    result, unanswered = srp(packet, timeout=2, verbose=False)
+
+    # 处理响应结果
+    for sent, received in result:
+        print(received.sprintf("MAC地址: %Ether.src%, IP地址: %ARP.psrc%"))
+
+
 if __name__ == '__main__':
-    r = local_mac("192.168.2.42")
-    print(f"local_mac {r}")
+    # r = local_mac("192.168.2.42")
+    # print(f"local_mac {r}")
+    #
+    # r = remote_mac("152.32.190.242")  # arp作用于局域网，跨网络会一直等待
+    # print(f"remote_mac {r}")
 
-    r = remote_mac("152.32.190.242")  # arp作用于局域网，跨网络会一直等待
-    print(f"remote_mac {r}")
-
+    arp_request_t()
