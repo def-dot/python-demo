e15d9cae60cee1d35d64dd318d4a9c873b909ffd
shijingjing <shijingjing07@126.com>
2024-01-09 19:13:31 +0800

    优先队列实现

--- suanfa_t/tanxin_zhuchiren.py
+++ suanfa_t/tanxin_zhuchiren.py
@@ -1,4 +1,8 @@
 # 贪心算法，主持人调度
+from typing import List
+import functools
+from queue import PriorityQueue
+
 class Solution:
     def min_host(self, nums):
         start = []
@@ -18,9 +22,22 @@ class Solution:
             else:
                 j += 1
         return r
-
+    
+    def min_host2(self, nums):
+        sorted_nums = sorted(nums, key=lambda x: (x[0], x[1]))
+        # [[1, 6], [2, 4], [4, 5]]
+        q = PriorityQueue()
+        q.put(-1)
+        for t in sorted_nums:
+            v = q.get()
+            if v > t[0]:
+                # 开始时间小于结束时间
+                q.put(v)
+            q.put(t[1])
+        return q.qsize()
+    
 
 if __name__ == "__main__":
-    plans = [[1,6],[2,4],[4,5]]
-    r = Solution().min_host(plans)
+    plans = [[4,5],[1,6],[2,4]]
+    r = Solution().min_host2(plans)
     print(r)
