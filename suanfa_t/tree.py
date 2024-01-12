b411d449131b073be5fd012bde6d6049cc0657d1
shijingjing <shijingjing07@126.com>
2024-01-12 10:39:39 +0800

    层序遍历

--- suanfa_t/tree.py
+++ suanfa_t/tree.py
@@ -1,3 +1,4 @@
+# 树 层序 前序 中序 后序 遍历
 class Tree:
     def __init__(self, val) -> None:
         self.val = val
@@ -16,7 +17,27 @@ class Solution:
             func(t.right)
         func(t)
         return res
-
+    
+    def level(self, t):
+        import queue
+        q = queue.Queue()
+        r = []
+        if not t:
+            return r
+        q.put(t)
+        while not q.empty():
+            c = []
+            n = q.qsize()
+            for i in range(n):
+                node = q.get()
+                c.append(node.val)
+                if node.left:
+                    q.put(node.left)
+                if node.right:
+                    q.put(node.right)
+            r.append(c)                
+        return r
+          
     def prev(self, t):
         if not t:
             return
@@ -43,9 +64,15 @@ if __name__ == '__main__':
     t1 = Tree(1)
     t2 = Tree(2)
     t3 = Tree(3)
+    t4 = Tree(4)
+    t5 = Tree(5)
     t1.left = t2
     t1.right = t3
+    t3.left = t4
+    t4.right = t5
     # Solution().prev(t1)
     # Solution().middle(t1)
     # Solution().post(t1)
-    Solution().prev2(t1)
+    # Solution().prev2(t1)
+    r = Solution().level(t1)
+    print(r)
